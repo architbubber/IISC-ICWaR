1f9fa145ea86582db74db4c2b91ea0021a89a315
architbubber <architbubber07@gmail.com>
2023-07-17 19:06:56 +0530

    Fixed Faculty page, now renders data automatically from ts file Added dropDown on Hover for desktop devices Renamed Chair to Chairman in Contact-us page

--- src/app/views/faculty/faculty.component.ts
+++ src/app/views/faculty/faculty.component.ts
@@ -4,35 +4,37 @@ import { faculty } from 'src/data-entries/json/faculty';
 // import  * as facultyData from '../../../data-entries/json/faculty.json';
 Injectable()
 
-export class FacultyData {
-  'Chairman' : FacultyInfo[]
-  'Core' :FacultyInfo[]
-  'Other' : FacultyInfo[]
-  constructor(Chairman : FacultyInfo[],
-  Core :FacultyInfo[],
-  Other : FacultyInfo[]){
-
-  }
+export interface FacultyData {
+  Chairman : FacultyInfo[];
+  Core :FacultyInfo[];
+  Other : FacultyInfo[];
+  // constructor(Chairman : FacultyInfo[],
+  // Core :FacultyInfo[],
+  // Other : FacultyInfo[]){
+  //   this.Chairman = Chairman;
+  //   this.Core = Core;
+  //   this.Other = Other;
+  // }
 }
-export class FacultyInfo {
-  name:String;
-  designation:String
+export interface FacultyInfo {
+  name:string;
+  designation:string
   department:String
   phoneNo:String
   email:String
   imgSrc:String
-  constructor(  name:String,    designation:String,
-    department:String,
-    phoneNo:String,
-    email:String,
-    imgSrc:String){
-      this.department = department;
-      this.designation=designation;
-      this.email = email;
-      this.imgSrc = imgSrc;
-      this.name = name;
-      this.phoneNo = phoneNo;
-    }
+  // constructor(  name:String,    designation:String,
+  //   department:String,
+  //   phoneNo:String,
+  //   email:String,
+  //   imgSrc:String){
+  //     this.department = department;
+  //     this.designation=designation;
+  //     this.email = email;
+  //     this.imgSrc = imgSrc;
+  //     this.name = name;
+  //     this.phoneNo = phoneNo;
+  //   }
 
 }
 
@@ -43,13 +45,24 @@ export class FacultyInfo {
 })
 
 export class FacultyComponent {
-  // facultyProcessed = FacultyData[] faculty: any;
-  designationNames :any;
-  staffUnderDesignation = [[]];
+
+  designationNames: string[]=[];
+  staffUnderDesignation : FacultyInfo[][]=[];
   ngOnInit(): void {
-
-
-    }
+    for(var i=0;i<faculty.length;i++){
+      let key = Object.keys(faculty[i])[0];
+      this.designationNames.push(key);
+      // console.log(Object.values(faculty[i])[])
+      let c  = Object.values(faculty[i]);
+     this.staffUnderDesignation.push(c[0]);
+   }
+  //  this.staffUnderDesignation.forEach(f=>{
+  //   f.forEach(r=>
+  //     console.log(r);
+  //   )
+  //  })
+  }
     // console.log(this.designationNames)
 
 }
+// var processd : FacultyData[] ;
--- src/data-entries/json/faculty.ts
+++ src/data-entries/json/faculty.ts
@@ -1,133 +1,142 @@
 export const faculty = [
-  {"Chairman":[{
-  "name":"Dr. Praveen C Ramamurthy",
-  "designation":"chairman",
-  "department":"Department of Materials Engineering",
-  "phoneNo":"+91 080 2293 2627",
-  "email":"praveen@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-}]},
-{"Core":[
-{
-  "name":"Dr. Rajarshi Das Bhowmik",
-  "designation":"Assistant Professor",
-  "department":"ICWaR",
-  "phoneNo":"+91 80 2293 3224",
-  "email":"rajarshidb@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Dr. Bramha Dutt Vishwakarma",
-  "designation":"Assistant Professor",
-  "department":"ICWaR",
-  "phoneNo":"",
-  "email":"bramha@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-}
-]},
-{"Other":[{
-  "name":"Prof. P. P. Mujumdar",
-  "designation":"Associate Faculty",
-  "department":"Department of Civil Engineering",
-  "phoneNo":"+91 80 2293 2669",
-  "email":"pradeep@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Prof. Govindasamy Bala",
-  "designation":"",
-  "department":"Centre for Atmospheric & Oceanic Sciences",
-  "phoneNo":"+91 80 2293 3428",
-  "email":"gbala@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Prof. Sekhar M.",
-  "designation":"",
-  "department":"Department of Civil Engineering",
-  "phoneNo":"+91 80 2293 2245",
-  "email":"muddu@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Prof. Srinivas V. V.",
-  "designation":"",
-  "department":"Department of Civil Engineering",
-  "phoneNo":"+ 91 80 2293 2641",
-  "email":"vvs@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Prof. Prosenjit Ghosh",
-  "designation":"",
-  "department":"Centre for Earth Sciences",
-  "phoneNo":"+91 80 2293 2599",
-  "email":"pghosh@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Dr. Ramananda Chakrabarti",
-  "designation":"",
-  "department":"Centre for Earth Sciences",
-  "phoneNo":"+91 80 2293 3003",
-  "email":"ramananda@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Dr Lakshminarayana Rao",
-  "designation":"",
-  "department":"Centre for Sustainable Technologies",
-  "phoneNo":"080 2293 2051",
-  "email":"narayana@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Dr.Sanjeev Sambandan",
-  "designation":"",
-  "department":"Instrumentation And Applied Physics",
-  "phoneNo":"Tel: 080 2293  3196",
-  "email":"sanjiv@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Dr. Thejna Tharammal",
-  "designation":"DST Insprire Faculty",
-  "department":"",
-  "phoneNo":"",
-  "email":"thejnat@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Dr. Soumita Boral ",
-  "designation":"DST Inspire Faculty",
-  "department":"",
-  "phoneNo":"",
-  "email":"soumitab@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Prof. Venugopal V.",
-  "designation":"",
-  "department":"Centre for Atmospheric and Oceanic Sciences",
-  "phoneNo":"+91 80 2293 3073",
-  "email":"venu@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Nagesh Kumar D",
-  "designation":"",
-  "department":"Department of Civil Engineering",
-  "phoneNo":"+91 80 2293 2666",
-  "email":"nagesh@iisc.ac.in",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-},
-{
-  "name":"Prof. Mohan Kumar M. S.",
-  "designation":"Retired Faculty",
-  "department":"",
-  "phoneNo":"",
-  "email":"",
-  "imgSrc":"../../assets/images/img_avatar3.png"
-}
-]}
-]
+  {
+    Chairman: [
+      {
+        name: "Dr. Praveen C Ramamurthy",
+        designation: "chairman",
+        department: "Department of Materials Engineering",
+        phoneNo: "+91 080 2293 2627",
+        email: "praveen@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+    ],
+  },
+  {
+    Core: [
+      {
+        name: "Dr. Rajarshi Das Bhowmik",
+        designation: "Assistant Professor",
+        department: "ICWaR",
+        phoneNo: "+91 80 2293 3224",
+        email: "rajarshidb@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Dr. Bramha Dutt Vishwakarma",
+        designation: "Assistant Professor",
+        department: "ICWaR",
+        phoneNo: "",
+        email: "bramha@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+    ],
+  },
+  {
+    'Associate & DST': [
+      {
+        name: "Prof. P. P. Mujumdar",
+        designation: "Associate Faculty",
+        department: "Department of Civil Engineering",
+        phoneNo: "+91 80 2293 2669",
+        email: "pradeep@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Prof. Govindasamy Bala",
+        designation: "",
+        department: "Centre for Atmospheric & Oceanic Sciences",
+        phoneNo: "+91 80 2293 3428",
+        email: "gbala@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Prof. Sekhar M.",
+        designation: "",
+        department: "Department of Civil Engineering",
+        phoneNo: "+91 80 2293 2245",
+        email: "muddu@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Prof. Srinivas V. V.",
+        designation: "",
+        department: "Department of Civil Engineering",
+        phoneNo: "+ 91 80 2293 2641",
+        email: "vvs@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Prof. Prosenjit Ghosh",
+        designation: "",
+        department: "Centre for Earth Sciences",
+        phoneNo: "+91 80 2293 2599",
+        email: "pghosh@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Dr. Ramananda Chakrabarti",
+        designation: "",
+        department: "Centre for Earth Sciences",
+        phoneNo: "+91 80 2293 3003",
+        email: "ramananda@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Dr Lakshminarayana Rao",
+        designation: "",
+        department: "Centre for Sustainable Technologies",
+        phoneNo: "080 2293 2051",
+        email: "narayana@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Dr.Sanjeev Sambandan",
+        designation: "",
+        department: "Instrumentation And Applied Physics",
+        phoneNo: "Tel: 080 2293  3196",
+        email: "sanjiv@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Dr. Thejna Tharammal",
+        designation: "DST Insprire Faculty",
+        department: "",
+        phoneNo: "",
+        email: "thejnat@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Dr. Soumita Boral ",
+        designation: "DST Inspire Faculty",
+        department: "",
+        phoneNo: "",
+        email: "soumitab@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Prof. Venugopal V.",
+        designation: "",
+        department: "Centre for Atmospheric and Oceanic Sciences",
+        phoneNo: "+91 80 2293 3073",
+        email: "venu@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Nagesh Kumar D",
+        designation: "",
+        department: "Department of Civil Engineering",
+        phoneNo: "+91 80 2293 2666",
+        email: "nagesh@iisc.ac.in",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+      {
+        name: "Prof. Mohan Kumar M. S.",
+        designation: "Retired Faculty",
+        department: "",
+        phoneNo: "",
+        email: "",
+        imgSrc: "../../assets/images/img_avatar3.png",
+      },
+    ],
+  },
+];
