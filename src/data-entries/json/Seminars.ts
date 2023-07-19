fbac97b144229dde1085e8a058fc93e04db393ed
architbubber <architbubber07@gmail.com>
2023-07-19 12:47:04 +0530

    Added new Seminar json file

---
+++ src/data-entries/json/Seminars.ts
@@ -0,0 +1,11 @@
+export const seminars = [
+  {
+    'title':"",
+    'name of the speaker':'',
+    'Title of the Seminar':'',
+    'Date and Time':'',
+    'Online Platform':'',
+    'About the Speaker':'',
+    'Abstract':''
+  }
+]
