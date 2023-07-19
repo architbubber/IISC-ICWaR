a5bb8bbccc2e17329eca5f1054793979c59b28f0
architbubber <architbubber07@gmail.com>
2023-07-19 13:21:30 +0530

    Added new imgsArray in monsoon-school.ts

--- src/data-entries/json/monsoon-schools.ts
+++ src/data-entries/json/monsoon-schools.ts
@@ -1,23 +1,38 @@
 export const monsoonPageData=[
     {
     '2017':{
-
+        imgsArray:[
+          '',
+          ''
+        ]
       }
     },
     {
     '2018':{
-
+      imgsArray:[
+        '',
+        ''
+      ]
       }
     },
     {
     '2019':{
-
+      imgsArray:[
+        '',
+        ''
+      ]
       }
     },
     {
       '2023':{
-
+        imgsArray:[
+          '',
+          ''
+        ]
         }
     },
 
 ]
+//Monsoon button opens a modal with button for each year
+//Each year defines a tab with addition tab for form
+//Onclick opens monsoon school for that year
