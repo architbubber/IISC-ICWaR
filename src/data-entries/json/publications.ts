ca9771b73319077a07796f31d40cf5e63583b204
architbubber <architbubber07@gmail.com>
2023-07-20 18:38:38 +0530

    Added new publications list json

---
+++ src/data-entries/json/publications.ts
@@ -0,0 +1,15 @@
+export const publications = [
+  {
+    'Replace_With_Professor_Name':{
+      'websiteLink':'',
+      'publicationsList':[
+        {
+          'text':'',
+          'links':[
+            '',''
+          ]
+        }
+      ]
+    }
+  }
+]
