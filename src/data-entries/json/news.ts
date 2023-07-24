7ed64b2864a961beaf8cd99bde2ce01832e469e9
krispy <krishnapandeykp@users.noreply.github.com>
2023-07-24 16:35:06 +0530

    Updated Gallery Data and RedirectTo in News.ts

--- src/data-entries/json/news.ts
+++ src/data-entries/json/news.ts
@@ -8,42 +8,50 @@ export const newsJson = [{
 {
   title:"Deccan Herald – Sept 16, 2022",
   date:"16/09/2022",
-  imgSrc:"../../../assets/images/News/bangaluru-flood-prediction.jpg"
+  imgSrc:"../../../assets/images/News/bangaluru-flood-prediction.jpg",
+  redirectTo:'https://www.deccanherald.com/specials/point-blank/bengaluru-to-get-flood-prediction-system-soon-1145776.html'
 },
 {
   title:"New Book Chapter: Hydrological status and outlooks co-authored by Prof. P P Mujumdar (Chairman, ICWaR), Dr. Katie Facer-Childs (CEH, UK), and Anusree K Anju (SRF, ICWaR) in Emerging science for sustainable water resource management, edited by Dr. Sunita Sarkar and Prof. Harry Dixon.",
   date:" ",
-  imgSrc:"../../../assets/images/News/New-Book-Chapter-Hydrological-status-outlook.png"
+  imgSrc:"../../../assets/images/News/New-Book-Chapter-Hydrological-status-outlook.png",
+  redirectTo:'https://www.ceh.ac.uk/sites/default/files/2021-11/sustainable-water-guide-chap6-hydrological-status-v2.pdf'
 },
 {
   title:"This is a crowd-funded initiative led by Mr. Sengupta where several ICWaR students participated. Bangalore Mirror – Sept 13, 2022",
   date:"13/09/2022",
-  imgSrc:"../../../assets/images/News/Life-Streaming-BM.png"
+  imgSrc:"../../../assets/images/News/Life-Streaming-BM.png",
+  redirectTo:'https://bangaloremirror.indiatimes.com/bangalore/cover-story/life-streaming/articleshow/94160368.cms'
 },
 {
   title:"“Urban Modelling: Development of multi-sectorial simulation lab and science-based decision support framework to address urban environmental issues” is a collaborative project under National Super-computing Mission with C-DAC as the lead organisation, funded by DST. ICWaR is contributing to the hydrological aspects of the project.",
   date:" ",
-  imgSrc:"../../../assets/images/News/Urban-Modelling.jpg"
+  imgSrc:"../../../assets/images/News/Urban-Modelling.jpg",
+  redirectTo:''
 },
 {
   title:"Initiative by ICWaR in collaboration with UNESCO, to provide information and data on everything related to climate change and downscaling for urban areas for the benefit of researchers, students, policymakers and practitioners worldwide. Click here to visit the Urban Climate Downscaling Portal (UCDP)",
   date:" ",
-  imgSrc:"../../../assets/images/News/climate-change-and-downscaling.jpg"
+  imgSrc:"../../../assets/images/News/climate-change-and-downscaling.jpg",
+  redirectTo:'http://ucdp.icwar.iisc.ac.in/'
 },
 {
   title:"Discussion Meeting held on “Scale Issues in Hydrology“,  during December 1-3, 2019, in Hoysala Village Resort, Hassan. Click here for a detailed report.",
   date:"03/12/2019",
-  imgSrc:"../../../assets/images/News/scaleissues-hydrology.jpg"
+  imgSrc:"../../../assets/images/News/scaleissues-hydrology.jpg",
+  redirectTo:'http://icwar.iisc.ac.in/wp-content/uploads/2019/01/Discussion-Meeting.pdf'
 },
 {
   title:"A four-week short course was held by Prof. Murugesu Sivapalan, Chester and Helen Siess Endowed Professor in Civil and Environmental Engineering, University of Illinois at Urbana Champaign, USA on the topic “Catchment Hydrology: A Synthesis of Time and Space Scales” October 21, 2019, in ICWaR Lecture Hall.",
   date:"21/10/2019",
-  imgSrc:"../../../assets/images/News/prof.Sivapalan.png"
+  imgSrc:"../../../assets/images/News/prof.Sivapalan.png",
+  redirectTo:''
 },
 {
   title:"IISc Press- INSA book on “Water Futures of India-status of science and technology”  edited by P P Mujumdar (Chairman, ICWaR) & V M Tiwari (Director, NGRI)",
   date: "15/03/2019",
-  imgSrc:"../../../assets/images/News/book-cover-insawater.jpg"
+  imgSrc:"../../../assets/images/News/book-cover-insawater.jpg",
+  redirectTo:'https://www.researchgate.net/publication/331438542_Water_Futures_of_India_Status_of_Science_and_Technology_Edited_Volume'
 }
 
 ]
