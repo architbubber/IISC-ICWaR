cc3e6ae82acc04e8edaca0c3d1f04772811ad1f7
architbubber <architbubber07@gmail.com>
2023-10-09 10:32:19 +0530

    Navigation bar is now dynamic generated. Fixed font-family for different pages to global one

--- src/data-entries/config.ts
+++ src/data-entries/config.ts
@@ -1,8 +1,8 @@
 /*******************************************************************************************************************************/
 //                                                  !!! ATTENTION !!!                                                          //
 //                                                                                                                             //
-//             /\                    THIS IS A VOLATILE AND SENSITIVE FILE. ONLY AUTHORISED                    /\              //
-//            /  \                   AND INDIVIDUAL FAMILIAR WITH WORKING OF THE SITE SHOULD                  /  \             //
+//             /\                    THIS IS A HIGHLY VOLATILE FILE. ONLY AUTHORISED                           /\              //
+//            /  \                   PERSONNEL FAMILIAR WITH WORKING OF THE SITE SHOULD                       /  \             //
 //           /    \                  MAKE ANY CHANGES TO THIS FILE.                                          /    \            //
 //          /  |   \                                                                                        /  |   \           //
 //         /   |    \                                                                                      /   |    \          //
@@ -17,7 +17,7 @@ export const configurations = {
 
   "global-settings":{
     "font-family":'Sabon-next-lt',
-    "font-size":'20px'
+    "font-size":'15px'
   },
 
   "spreeMailUrl":"https://formspree.io/f/xzblzjgk",
@@ -37,6 +37,60 @@ export const configurations = {
       // 'min-height': '10vh',
       'font-size':'25px'
     },
+    "navMenu":[
+      {
+        "name":"Home",
+        "link":"#"
+      },
+      {
+        "name":"Research",
+        "link":"/research"
+      },
+      {
+        "name":"People",
+        "hasDropdown":true,
+        "dropdownStyle":{
+          'color':'white',
+          'background-color': '#739ad9',
+          'border-top':'none',
+          'font-size':'25px'
+        },
+        "dropdownItems":[
+          {
+            "name":"Faculty",
+            "link":"/people?type=faculty"
+          },
+          {
+            "name":"Visitors",
+            "link":"/people?type=visitors"
+          },
+          {
+            "name":"Research Students",
+            "link":"/people?type=researchStudents"
+          },
+          {
+            "name":"Post Doctoral Scholars",
+            "link":"/people?type=postDoctoralScholars"
+          },
+          {
+            "name":"Project Staff",
+            "link":"/people?type=projectStaff"
+          },
+          {
+            "name":"Office Staff",
+            "link":"/people?type=officeStaff"
+          },
+        ],
+      },
+      {
+        "name":"Collaborations",
+        "link":"/Collaborations"
+      },
+      {
+        "name":"Publications",
+        "link":"/publications"
+      }
+    ],
     "navLinks":{
         // 'font-weight': 'bolder',
         'color':'white',
